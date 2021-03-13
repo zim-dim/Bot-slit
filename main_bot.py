5bc7641fd2d57690be6cced5805d79662187e5b5
Volmodav <80148914+Volmodav@users.noreply.github.com>
2021-03-13 10:09:51 +0200

    Update main_bot.py

--- main_bot.py
+++ main_bot.py
@@ -323,7 +323,21 @@ def main():
     dp.add_error_handler(error)
     updater.start_polling()
     updater.idle()
+    
+    dp.add_handler(CallbackQueryHandler(vykladach,
+                                        pattern ='Викладачі'))
 
+    dp.add_handler(CallbackQueryHandler(vidmin_kaf,
+                                        pattern ='Відомості о Кафедрі'))
+
+    dp.add_handler(CallbackQueryHandler(hist_kaf,
+                                        pattern ='Історична Кафедра'))
+
+    dp.add_handler(CallbackQueryHandler(aud_kaf,
+                                        pattern ='Аудеторія Кафедри'))
+
+    dp.add_handler(CallbackQueryHandler(n_vypuskniki,
+                                        pattern ='Наші Випусники'))
 
 if __name__ == '__main__':
     main()
